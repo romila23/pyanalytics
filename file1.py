986840fa476c75e2f3e6cb62a7791e3254547ea6
romila23 <136985649+romila23@users.noreply.github.com>
2023-06-18 22:13:52 +0530

    18Jun Romila

---
+++ file1.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+x = [1,2,3]
+x
+print(x)
+y = ['Romila', 'Árun', 'Himani','Shikhar']
+y
+z = [1,2,3,'romila']
+z
